1d4ac4fc8822af1dc18819739c127ab4f66658e3
vahaponur <vahaponuryildirim@gmail.com>
2022-01-07 02:37:32 +0300

    Odev1

---
+++ ClassLibrary1/Odev1/Odev1P.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ClassLibrary1.Odev1
+{
+    public static class Odev1P
+    {
+        public static void Soru1()
+        {
+            Console.WriteLine("Array boyutu giriniz");
+            int.TryParse(Console.ReadLine(),out int n);
+            var array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int sayi);
+                array[i] = sayi;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]%2==0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        public static void Soru2()
+        {
+            Console.WriteLine("Array Boyutu:");
+            int.TryParse(Console.ReadLine(), out int n);
+            Console.WriteLine("kontrol sayisi");
+            int.TryParse(Console.ReadLine(), out int m);
+            var array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int sayi);
+                array[i] = sayi;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % m == 0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        public static void Soru3()
+        {
+            Console.WriteLine("Array boyutu giriniz");
+            int.TryParse(Console.ReadLine(), out int n);
+            var array = new string[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+             Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static void Soru4()
+        {
+            string cumle = Console.ReadLine();
+            string[] kelimeler = cumle.Split(" ");
+            int harfSayisi = 0;
+            foreach (var item in kelimeler)
+            {
+                Console.WriteLine(item);
+                foreach (var harf in item)
+                {
+                    harfSayisi++;
+                }
+            }
+            Console.WriteLine("Harf Sayisi: "+harfSayisi);
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,13 @@
 ﻿using ClassLibrary1;
 using System;
-
+using ClassLibrary1.Odev1;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] D1 = { 1, 2, 3, 5, 7 };
-            int[] D2 = { 10, 20, 30, 50, 70 };
-            int[] D3 = Class1.Merge(D1, D2);
-            foreach (var item in D3)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
+            Odev1P.Soru4();
         }
 
     }
